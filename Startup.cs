adc7c1f1def364f07ba15b96dd3d6685fccc0bad
Yue Song <ysong@commonwealthcare.org>
2018-12-28 11:02:18 -0500

    extend user model, add user repo, add user repo to service

--- Startup.cs
+++ Startup.cs
@@ -33,9 +33,10 @@ namespace DatingApp.API
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddCors();
+            services.AddTransient<Seed>();
             // create a instance for each request
             services.AddScoped<IAuthRepository, AuthRepository>();
-
+            services.AddScoped<IDatingRepository, DatingRepository>();
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(options => {
                     options.TokenValidationParameters = new TokenValidationParameters{
@@ -49,7 +50,7 @@ namespace DatingApp.API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, Seed seeder)
         {
             if (env.IsDevelopment())
             {
@@ -63,6 +64,9 @@ namespace DatingApp.API
 
             app.UseHttpsRedirection();
             // allow cors by adding cors header in http respond, this should add to pipeline befroe use MVC
+            
+            // this used when need seed, before this called, run dotnet ef database drop/update
+            // seeder.SeedUsers();
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseAuthentication();
             app.UseMvc();
